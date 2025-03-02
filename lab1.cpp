65e620090589e9bfef543587c85c08237e13f8de
england61300 <126538708+england61300@users.noreply.github.com>
2025-03-02 13:39:34 -0800

    Create lab1.cpp
    test 1

---
+++ lab1.cpp
@@ -0,0 +1,17 @@
+//This little program converts from pounds to kilograms
+//Devin England
+//Section#: 16836
+//Lab 1
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+  string name;
+  double pounds, kg;
+  cout << "Devin: ";
+  cin >> pounds;
+  kg = pounds / 2.205;
+  cout << name << ", 180 pounds " << kg << endl;
+  return 0;
+}
