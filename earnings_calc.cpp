4e6073e730081874e6f1801195e69951a54155cd
england61300 <126538708+england61300@users.noreply.github.com>
2025-03-02 14:15:20 -0800

    Create earnings_calc.cpp

---
+++ earnings_calc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int candyBarsSold;
+    double earningsPerBar, totalEarnings;
+
+    // Input
+    cout << "Enter the number of candy bars sold: ";
+    cin >> candyBarsSold;
+    cout << "Enter the amount earned per candy bar: ";
+    cin >> earningsPerBar;
+
+    // Processing
+    totalEarnings = candyBarsSold * earningsPerBar;
+
+    // Output
+    cout << "The total amount earned is: $" << totalEarnings << endl;
+
+    return 0;
+}
